1e519d4d717353b975b83c37e423ed9f7744698a
barisc97 <41056158+barisc97@users.noreply.github.com>
2018-07-10 15:12:11 +0300

    Add files via upload

---
+++ TheCode.java
@@ -0,0 +1,15 @@
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+public class TheCode {
+public static void main(String[] args) {
+try{
+
+PrintStream out = new PrintStream(new FileOutputStream("outputOfCode.txt"));
+System.setOut(out);
+int[] arr = [13, 5, 2, 8, 6];
+public int SelectionSort(){
+int[] arr2 = [2, 5, 6, 8, 13];
+}
+SelectionSort();
+}catch(Exception e){System.out.println();}
+}}
